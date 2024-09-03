7ff40e93f2e511c030062fa4207284983003e5b6
Akshay Kumar Maurya <akshay21sep@gmail.com>
2024-09-03 23:55:29 +0530

    Kotlin Coroutines - GlobalScope.lanuch

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+//            delay(3000L) // Delay/Sleep Thread for 3 second.
+//            If one coroutines pauses/delay it would not affect other coroutines. other thread will just keep working. (If coroutines are pause/delayed which would equivalent Thread.sleep().
+//            Note: if main thread finishes with his work then that means all other thread and coroutines will be cancelled so even though they are started  in an another thread and are executed asynchronously the will be cancelled if the main thread finishes his work.
+//            we can see that if we increase the delay to 5 second from 3 second.
+            delay(5000L)
+//            if we quit the app other thread and coroutines will be cancelled.
+            Log.d(TAG, "onCreate: Coroutines say hello from thread ${Thread.currentThread().name} ")
+        }
+        Log.d(TAG, "onCreate: hello from thread ${Thread.currentThread().name} ")
+
     }
 }
